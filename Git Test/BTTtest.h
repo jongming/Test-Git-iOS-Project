c0d80d6fc239e30d26555e2a8925d7e8568e00c7
Benjamin Tai <benjamintai@Benjamins-MacBook-Pro.local>
2014-05-12 22:58:40 +0900

    added test class

---
+++ Git Test/BTTtest.h
@@ -0,0 +1,13 @@
+//
+//  BTTtest.h
+//  Git Test
+//
+//  Created by Benjamin Tai on 5/12/14.
+//  Copyright (c) 2014 Tai Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTTtest : NSObject
+
+@end
---
+++ Git Test/BTTtest.m
@@ -0,0 +1,13 @@
+//
+//  BTTtest.m
+//  Git Test
+//
+//  Created by Benjamin Tai on 5/12/14.
+//  Copyright (c) 2014 Tai Inc. All rights reserved.
+//
+
+#import "BTTtest.h"
+
+@implementation BTTtest
+
+@end
